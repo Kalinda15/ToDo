17ea2a1df569dcf9d725e9879579726da39aabc1
Kalinda15 <ricardodeca08@gmail.com>
2023-03-13 22:56:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+class ToDo extends Component{
+    state={
+        tarefa:"",
+        lista:[]
+    };
+    tarefa=(e)=>{
+        this.setState({
+            tarefa:e.target.value
+        })
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    enviar=()=>{
+        if(this.state.tarefa === ''){
+            return false
+        }else{
+            this.setState({
+                lista: this.state.lista.concat({
+                    tarefa:this.state.tarefa,
+                    id: Date.now()
+                }),
+                tarefa:''
+            })
+        }
+
+    }
+    delete = (id) => {
+        this.setState({
+            lista: this.state.lista.filter((item) => item.id !== id)
+        });
+    };
+    deleteAll = () => {
+        this.setState({
+            lista : this.state.lista.filter((item) => item.lista)
+        });
+    }
+    render(){
+        return(
+             <form onSubmit={(e)=>e.preventDefault()}>
+             <h1>MINHA LISTA - TO DO</h1>
+             <input onChange={this.tarefa} value={this.state.tarefa} type='text'/>
+             <button onClick={this.enviar}>ADICIONAR</button>
+             <button onClick={()=>this.deleteAll(this.state.lista)}>APAGAR ITENS</button>
+             {this.state.lista.map((item,index)=>(
+                <div key={index}>
+                <ul>
+                    <li>{item.tarefa}</li>
+                    <button onClick ={() => this.delete(item.id)}>X</button>
+                </ul>
+                </div>
+             ))}
+             </form>
+        )
+    }
 }
-
-export default App;
+export default ToDo

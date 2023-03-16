c9eab816543fae67cc9fe39d1d2df4d3d2cba6fe
Kalinda15 <ricardodeca08@gmail.com>
2023-03-16 14:04:05 -0300

    update css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import "./App.css"
 class ToDo extends Component{
     state={
         tarefa:"",
@@ -37,15 +38,16 @@ class ToDo extends Component{
     render(){
         return(
              <form onSubmit={(e)=>e.preventDefault()}>
-             <h1>MINHA LISTA - TO DO</h1>
+             <h1>TO DO</h1>
+             <h2>MINHA LISTA DE COMPRAS</h2>
              <input onChange={this.tarefa} value={this.state.tarefa} type='text'/>
              <button onClick={this.enviar}>ADICIONAR</button>
-             <button onClick={()=>this.deleteAll(this.state.lista)}>APAGAR ITENS</button>
+             <button class="apagar" onClick={()=>this.deleteAll(this.state.lista)}>APAGAR ITENS</button>
              {this.state.lista.map((item,index)=>(
                 <div key={index}>
                 <ul>
                     <li>{item.tarefa}</li>
-                    <button onClick ={() => this.delete(item.id)}>X</button>
+                    <button class="x" onClick ={() => this.delete(item.id)}>X</button>
                 </ul>
                 </div>
              ))}
